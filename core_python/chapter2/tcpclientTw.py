10c5f45446a84ea6e53fcfd80080fe8c8ae55692
e450c-linux <1819644289@qq.com>
2018-11-28 15:19:09 +0800

    modified:   chapter2/tcpclientTw.py 	modified:   chapter2/tcpserverTw.py

--- core_python/chapter2/tcpclientTw.py
+++ core_python/chapter2/tcpclientTw.py
@@ -8,22 +8,23 @@ HOST = '10.1.108.52'
 PORT = 21567
 
 class TSClntProtocol(protocol.Protocol):
+    #私有方法，建立连接后调用此函数发送数据
     def sendData(self):
-        data = naw_input('> ')
+        data = raw_input('> ')
         if data:
             print '...sending %s...' % data
             self.transport.write(data)
         else:
             self.transport.loseConnection()
-
+    #重写此方法，建立连接后调用
     def connectionMade(self):
         self.sendData()
-
+    #重写此方法，接收数据后调用
     def dataReceived(self, data):
         print data
         self.sendData()
 
-class TSClntFacotry(protocol.ClientFactory):
+class TSClntFactory(protocol.ClientFactory):
     protocol = TSClntProtocol
     clientConnectionLost = clientConnectionFailed = \
             lambda self, connector, reason: reactor.stop()
--- core_python/chapter2/tcpserverTw.py
+++ core_python/chapter2/tcpserverTw.py
@@ -8,9 +8,11 @@ from time import ctime
 PORT = 21567
 
 class TSServProtocol(protocol.Protocol):
+    #有客户端连接，调用此方法
     def connectionMade(self):
         clnt = self.clnt = self.transport.getPeer().host
         print '...connected from:', clnt
+    #收到客户端发来的数据，调用此方法
     def dataReceived(self, data):
         self.transport.write('[%s] %s' % (ctime(), data))
 
